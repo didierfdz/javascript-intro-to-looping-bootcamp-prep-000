8169eda50b351f6008c2318b3611368cc791058e
Carlos Didier Fernandez <didierfdz@gmail.com>
2019-06-07 19:53:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,11 +23,17 @@ function whileLoop(n) {
   return 'done';
 }
 
-function doWhileLoop(n)
+function doWhileLoop(num) {
   var i = 0;
  
   function incrementVariable() {
    i = i + 1;
     return i;
   }
- 
+  
+  do {
+    console.log('I run once regardless');
+  } while
+  (incrementVariable() < num)
+  }
+}
